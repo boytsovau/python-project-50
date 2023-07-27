06b20ac2bd02a31036090a9248861aaa46969b71
boytsovau <alexbonch@gmail.com>
2023-07-27 11:32:40 +0300

    modified:   Makefile 	modified:   gendiff/main.py

--- gendiff/main.py
+++ gendiff/main.py
@@ -2,9 +2,11 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference')
+    parser = argparse.ArgumentParser(description='Compares two configuration \
+                                                files and shows a difference')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    parser.add_argument('--f', '--format', metavar='FORMAT', help='set format of output')
 
     args = parser.parse_args()
     print(args.accumulate(args.file))
