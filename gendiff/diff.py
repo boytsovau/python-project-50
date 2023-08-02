5548fdff527d8ac3fbf8762a785bd231e35c664d
boytsovau <alexbonch@gmail.com>
2023-08-02 10:31:06 +0300

    modified:   gendiff/diff.py

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -3,23 +3,25 @@ import json
 
 
 def generate_diff(file1, file2):
-    data = []
     data1 = json.load(open(file1))
     data2 = json.load(open(file2))
-    data.append('{')
+    diff = {}
+
     for k, v in data1.items():
-        if k in data2.keys() and v == data2.get(k):
-            data.append(f'  {k}: {v}')
-        if k in data2.keys() and v != data2.get(k):
-            data.append(f'- {k}: {v}')
-            data.append(f'+ {k}: {data2.get(k)}')
-        if k not in data2.keys():
-            data.append(f'- {k}: {v}')
+        if k in data2:
+            if v == data2[k]:
+                diff[f'  {k}'] = v
+            else:
+                diff[f'- {k}'] = v
+                diff[f'+ {k}'] = data2[k]
+        else:
+            diff[f'- {k}'] = v
+
     for k2, v2 in data2.items():
-        if k2 not in data1.keys():
-            data.append(f'+ {k2}: {v2}')
-    data.append('}')
-    return '\n'.join(data)
+        if k2 not in data1:
+            diff[f'+ {k2}'] = v2
+
+    return '{\n' + '\n'.join([f'{key}: {val}' for key, val in diff.items()]) + '\n}'
 
 
 def main():
