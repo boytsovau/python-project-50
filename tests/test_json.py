f5f18ce537b55c5ba8b837611979d1f7f3b0a289
boytsovau <alexbonch@gmail.com>
2023-08-02 22:06:33 +0300

    new file:   tests/fixtures/expected.txt 	renamed:    tests/file1.json -> tests/fixtures/file1.json 	renamed:    tests/file2.json -> tests/fixtures/file2.json 	modified:   tests/test_json.py

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,12 +1,7 @@
 from gendiff.diff import generate_diff
 
+
 def test_json():
-    expected = '''{
-  host: hexlet.io
-- timeout: 50
-+ timeout: 20
-- proxy: 123.234.53.22
-- follow: False
-+ verbose: True
-}'''
-    assert generate_diff("tests/file1.json", "tests/file2.json") == expected
+    with open("tests/fixtures/expected.txt", 'r') as file:
+        expected = file.read()
+    assert generate_diff("tests/fixtures/file1.json", "tests/fixtures/file2.json") == expected
