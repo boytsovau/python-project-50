32d5730e340b9b996bd20651259660d7514b436e
boytsovau <alexbonch@gmail.com>
2023-08-31 16:19:05 +0300

    modified:   tests/test_diff.py

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -11,22 +11,22 @@ def test_json():
 def test_yml():
     with open("tests/fixtures/expected.txt", 'r') as file:
         expected = file.read()
-    diff = generate_diff("tests/fixtures/file1.yml", "tests/fixtures/file2.yaml")
-    assert diff == expected
+        diff = generate_diff("tests/fixtures/file1.yml", "tests/fixtures/file2.yaml")
+        assert diff == expected
 
 
 def test_json_stylish():
     with open("tests/fixtures/expected3.txt", 'r') as file:
         expected = file.read()
-    diff = generate_diff("tests/fixtures/file3.json", "tests/fixtures/file4.json")
-    assert diff == expected
+        diff = generate_diff("tests/fixtures/file3.json", "tests/fixtures/file4.json")
+        assert diff == expected
 
 
 def test_json_plain():
     with open("tests/fixtures/expected4.txt", 'r') as file:
         expected = file.read()
-    diff = generate_diff("tests/fixtures/file3.json", "tests/fixtures/file4.json", 'plain')
-    assert diff == expected
+        diff = generate_diff("tests/fixtures/file3.json", "tests/fixtures/file4.json", 'plain')
+        assert diff == expected
 
 
 
