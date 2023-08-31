6abc35e79f2cfe5678cb9292b72c06c02438ba73
boytsovau <alexbonch@gmail.com>
2023-08-31 16:16:47 +0300

    modified:   gen_diff/formaters/stylish.py

--- gen_diff/formaters/stylish.py
+++ gen_diff/formaters/stylish.py
@@ -16,27 +16,24 @@ def to_string(value, depth=1):
     return result
 
 
-def stylish_format(diff):
-    result = []
+result = []
 
-    def inner_format(data, depth=1):
-        for key, val in data.items():
-            action = val.get('action')
-            match action:
-                case 'nested':
-                    result.append(f"{get_offset(depth)}  {key}: {{")
-                    inner_format(val['children'], depth + 1)
-                    result.append(f"{get_offset(depth)}  }}")
-                case 'unchanged':
-                    result.append(f"{get_offset(depth)}  {key}: {to_string(val['value'], depth)}")
-                case 'update':
-                    result.append(f"{get_offset(depth)}- {key}: {to_string(val['old_value'], depth)}")
-                    result.append(f"{get_offset(depth)}+ {key}: {to_string(val['new_value'], depth)}")
-                case 'delete':
-                    result.append(f"{get_offset(depth)}- {key}: {to_string(val['value'], depth)}")
-                case 'added':
-                    result.append(f"{get_offset(depth)}+ {key}: {to_string(val['value'], depth)}")
-        inner_format(diff)
-        return '{\n' + '\n'.join(result) + '\n}'
 
-    return inner_format(diff)
+def stylish_format(data, depth=1):
+    for key, val in data.items():
+        action = val.get('action')
+        match action:
+            case 'nested':
+                result.append(f"{get_offset(depth)}  {key}: {{")
+                stylish_format(val['children'], depth + 1)
+                result.append(f"{get_offset(depth)}  }}")
+            case 'unchanged':
+                result.append(f"{get_offset(depth)}  {key}: {to_string(val['value'], depth)}")
+            case 'update':
+                result.append(f"{get_offset(depth)}- {key}: {to_string(val['old_value'], depth)}")
+                result.append(f"{get_offset(depth)}+ {key}: {to_string(val['new_value'], depth)}")
+            case 'delete':
+                result.append(f"{get_offset(depth)}- {key}: {to_string(val['value'], depth)}")
+            case 'added':
+                result.append(f"{get_offset(depth)}+ {key}: {to_string(val['value'], depth)}")
+    return '{\n' + '\n'.join(result) + '\n}'
