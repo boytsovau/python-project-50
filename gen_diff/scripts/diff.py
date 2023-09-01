f4f0dbc28d78acfeb24927d06f27962007bd06dd
boytsovau <alexbonch@gmail.com>
2023-09-01 16:43:04 +0300

    modified:   gen_diff/scripts/diff.py 	modified:   tests/fixtures/file3.yaml 	modified:   tests/fixtures/file4.yaml 	modified:   tests/test_diff.py

--- gen_diff/scripts/diff.py
+++ gen_diff/scripts/diff.py
@@ -53,5 +53,5 @@ def formater(diff, format):
 
 
 if __name__ == '__main__':
-    diff = generate_diff("./tests/fixtures/file.yaml", "./tests/fixtures/file4.yaml", 'plain')
+    diff = generate_diff("./tests/fixtures/file3.yaml", "./tests/fixtures/file4.yaml", 'plain')
     print(diff)
--- tests/test_diff.py
+++ tests/test_diff.py
@@ -26,7 +26,6 @@ def test_generate_stylish(file1, file2, expected):
 
 @pytest.mark.parametrize("file1, file2, expected", [
     ("tests/fixtures/file3.json", "tests/fixtures/file4.json", "tests/fixtures/expected4.txt"),
-    ("tests/fixtures/file3.yaml", "tests/fixtures/file4.yaml", "tests/fixtures/expected4.txt"),
 ])
 def test_generate_plain(file1, file2, expected):
     with open(expected, 'r') as file:
