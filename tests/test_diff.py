98eaa5bc7f47af407df79247a8324db52841c9ff
boytsovau <alexbonch@gmail.com>
2023-09-01 15:40:52 +0300

    Revert "	modified:   tests/test_diff.py"
    This reverts commit b54e845e31000714692ef781d7e88a0114af67c1.

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,16 +1,36 @@
-import pytest
 from gen_diff.scripts.diff import generate_diff
 
 
-@pytest.mark.parametrize("file1, file2, expected", [
-    ("tests/fixtures/file1.json", "tests/fixtures/file2.json", "tests/fixtures/expected.txt"),
-    ("tests/fixtures/file1.yml", "tests/fixtures/file2.yaml", "tests/fixtures/expected.txt"),
-    ("tests/fixtures/file3.json", "tests/fixtures/file4.json", "tests/fixtures/expected3.txt"),
-    ("tests/fixtures/file3.json", "tests/fixtures/file4.json", "tests/fixtures/expected4.txt"),
-    ("tests/fixtures/file3.json", "tests/fixtures/file4.json", "tests/fixtures/expected5.txt")
-])
-def test_generate_diff(file1, file2, expected):
-    with open(expected, 'r') as file:
-        expected_result = file.read()
-    diff = generate_diff(file1, file2)
-    assert diff == expected_result
+def test_json():
+    with open("tests/fixtures/expected.txt", 'r') as file:
+        expected = file.read()
+    diff = generate_diff("tests/fixtures/file1.json", "tests/fixtures/file2.json") 
+    assert diff == expected
+
+
+def test_yml():
+    with open("tests/fixtures/expected.txt", 'r') as file:
+        expected = file.read()
+    diff = generate_diff("tests/fixtures/file1.yml", "tests/fixtures/file2.yaml")
+    assert diff == expected
+
+
+def test_json_stylish():
+    with open("tests/fixtures/expected3.txt", 'r') as file:
+        expected = file.read()
+    diff = generate_diff("tests/fixtures/file3.json", "tests/fixtures/file4.json")
+    assert diff == expected
+
+
+def test_json_plain():
+    with open("tests/fixtures/expected4.txt", 'r') as file:
+        expected = file.read()
+    diff = generate_diff("tests/fixtures/file3.json", "tests/fixtures/file4.json", 'plain')
+    assert diff == expected
+
+
+def test_json_js():
+    with open("tests/fixtures/expected5.txt", 'r') as file:
+        expected = file.read()
+    diff = generate_diff("tests/fixtures/file3.json", "tests/fixtures/file4.json", 'json')
+    assert diff == expected
