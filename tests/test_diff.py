0482e7f13d20106b71262a7c3df504686f57f808
boytsovau <alexbonch@gmail.com>
2023-09-01 17:19:10 +0300

    renamed:    tests/fixtures/expected5.txt -> tests/fixtures/res_json.txt 	renamed:    tests/fixtures/expected4.txt -> tests/fixtures/res_json_plain.txt 	renamed:    tests/fixtures/expected3.txt -> tests/fixtures/res_json_stylish.txt 	renamed:    tests/fixtures/expected6.txt -> tests/fixtures/res_yaml_plain.txt 	new file:   tests/fixtures/res_yaml_stylish.txt 	modified:   tests/test_diff.py

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -16,28 +16,35 @@ def test_yml():
 
 
 def test_json_stylish():
-    with open("tests/fixtures/expected3.txt", 'r') as file:
+    with open("tests/fixtures/res_json_stylish.txt", 'r') as file:
         expected = file.read()
     diff = generate_diff("tests/fixtures/file3.json", "tests/fixtures/file4.json")
     assert diff == expected
 
 
+def test_yaml_stylish():
+    with open("tests/fixtures/res_yaml_stylish.txt", 'r') as file:
+        expected = file.read()
+    diff = generate_diff("tests/fixtures/file3.yaml", "tests/fixtures/file4.yaml", 'plain')
+    assert diff == expected
+
+
 def test_json_plain():
-    with open("tests/fixtures/expected4.txt", 'r') as file:
+    with open("tests/fixtures/res_json_plain.txt", 'r') as file:
         expected = file.read()
     diff = generate_diff("tests/fixtures/file3.json", "tests/fixtures/file4.json", 'plain')
     assert diff == expected
 
 
 def test_yaml_plain():
-    with open("tests/fixtures/expected6.txt", 'r') as file:
+    with open("tests/fixtures/res_yaml_plain.txt", 'r') as file:
         expected = file.read()
     diff = generate_diff("tests/fixtures/file3.yaml", "tests/fixtures/file4.yaml", 'plain')
     assert diff == expected
 
 
 def test_json_js():
-    with open("tests/fixtures/expected5.txt", 'r') as file:
+    with open("tests/fixtures/res_json.txt", 'r') as file:
         expected = file.read()
     diff = generate_diff("tests/fixtures/file3.json", "tests/fixtures/file4.json", 'json')
     assert diff == expected
