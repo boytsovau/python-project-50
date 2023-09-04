9d7f204967de8fbf211e6eee908404b608d6ca2e
boytsovau <alexbonch@gmail.com>
2023-09-04 15:08:39 +0300

    modified:   gendiff/format_check.py 	modified:   gendiff/formaters/plain.py 	modified:   gendiff/formaters/stylish.py

--- gendiff/format_check.py
+++ gendiff/format_check.py
@@ -1,12 +1,12 @@
-from gendiff.formaters.stylish import stylish_format
-from gendiff.formaters.plain import plain_format
+from gendiff.formaters.stylish import stylish
+from gendiff.formaters.plain import plain
 from gendiff.formaters.js import json_format
 
 
 def formater(diff, format):
     if format == 'plain':
-        return plain_format(diff)
+        return plain(diff)
     elif format == 'json':
         return json_format(diff)
     else:
-        return stylish_format(diff)
+        return stylish(diff)
--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -1,25 +1,28 @@
 from gendiff.core import get_offset
 
-result = []
 
+def stylish(data):
 
-def stylish_format(data, depth=1):
-    for key, val in data.items():
-        action = val.get('action')
-        match action:
-            case 'nested':
-                result.append(f"{get_offset(depth)}  {key}: {{")
-                stylish_format(val['children'], depth + 1)
-                result.append(f"{get_offset(depth)}  }}")
-            case 'unchanged':
-                result.append(f"{get_offset(depth)}  {key}: {to_string(val['value'], depth)}")
-            case 'update':
-                result.append(f"{get_offset(depth)}- {key}: {to_string(val['old_value'], depth)}")
-                result.append(f"{get_offset(depth)}+ {key}: {to_string(val['new_value'], depth)}")
-            case 'delete':
-                result.append(f"{get_offset(depth)}- {key}: {to_string(val['value'], depth)}")
-            case 'added':
-                result.append(f"{get_offset(depth)}+ {key}: {to_string(val['value'], depth)}")
+    result = []
+
+    def stylish_format(data, depth=1):
+        for key, val in data.items():
+            action = val.get('action')
+            match action:
+                case 'nested':
+                    result.append(f"{get_offset(depth)}  {key}: {{")
+                    stylish_format(val['children'], depth + 1)
+                    result.append(f"{get_offset(depth)}  }}")
+                case 'unchanged':
+                    result.append(f"{get_offset(depth)}  {key}: {to_string(val['value'], depth)}")
+                case 'update':
+                    result.append(f"{get_offset(depth)}- {key}: {to_string(val['old_value'], depth)}")
+                    result.append(f"{get_offset(depth)}+ {key}: {to_string(val['new_value'], depth)}")
+                case 'delete':
+                    result.append(f"{get_offset(depth)}- {key}: {to_string(val['value'], depth)}")
+                case 'added':
+                    result.append(f"{get_offset(depth)}+ {key}: {to_string(val['value'], depth)}")
+    stylish_format(data)
     return '{\n' + '\n'.join(result) + '\n}'
 
 
