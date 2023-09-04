253c38e599a8327b1f68490a6638c279d4689d98
boytsovau <alexbonch@gmail.com>
2023-09-04 15:14:46 +0300

    modified:   gendiff/core.py

--- gendiff/core.py
+++ gendiff/core.py
@@ -34,7 +34,7 @@ def parser_args():
                                             files and shows a difference')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('--f', '--format', metavar='FORMAT',
+    parser.add_argument('--f', '--format', '-f', metavar='FORMAT',
                         default='stylish', help='set format of output, \
                                             plain or json, stylish is default')
 
