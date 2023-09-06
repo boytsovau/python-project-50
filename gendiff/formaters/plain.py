e63ed91594a0cb33fe92e244991c28369e268bb5
boytsovau <alexbonch@gmail.com>
2023-09-06 14:26:53 +0300

    изменено:      gendiff/formaters/plain.py 	изменено:      gendiff/formaters/stylish.py

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -1,24 +1,24 @@
 def plain(data):
+    return plain_format(data)
 
+
+def plain_format(data, path=''):
     result = []
-
-    def plain_format(data, path=''):
-        for key, val in data.items():
-            action = val.get('action')
-            match action:
-                case 'nested':
-                    plain_format(val['children'], f'{path}{key}.')
-                case 'added':
-                    result.append(f"Property '{path}{key}' "
-                                  f"was added with value: "
-                                  f"{get_val(val['value'])}")
-                case 'delete':
-                    result.append(f"Property '{path}{key}' was removed")
-                case 'update':
-                    result.append(f"Property '{path}{key}' was updated. "
-                                  f"From {get_val(val['old_value'])} "
-                                  f"to {get_val(val['new_value'])}")
-    plain_format(data)
+    for key, val in data.items():
+        action = val.get('action')
+        match action:
+            case 'nested':
+                plain_format(val['children'], f'{path}{key}.')
+            case 'added':
+                result.append(f"Property '{path}{key}' "
+                              f"was added with value: "
+                              f"{get_val(val['value'])}")
+            case 'delete':
+                result.append(f"Property '{path}{key}' was removed")
+            case 'update':
+                result.append(f"Property '{path}{key}' was updated. "
+                              f"From {get_val(val['old_value'])} "
+                              f"to {get_val(val['new_value'])}")
     return '\n'.join(result)
 
 
